dd998031e7814d4b8ea7405f3cd956963f74dc3f
amrosnik <amrosnik@gmail.com>
2019-06-10 18:56:56 -0700

    for real commit all .py changes

--- adventure_cleaning.py
+++ adventure_cleaning.py
@@ -8,26 +8,87 @@ from lxml import html
 import numpy as np
 import re
 
-df = pd.read_pickle("./adventure_pickle.pkl")
+df = pd.read_pickle("./adventure_pickle_tupled.pkl")
 pd.set_option('display.max_rows',None)
 pd.set_option('display.max_columns',None)
 pd.set_option('display.expand_frame_repr',False)
 pd.set_option('max_colwidth',-1)
 
-#file = open("character.test",'w')
-#file.write(str(df.groupby(df['Character'].str.lower()).count()))
+df['Character'] = df['Character'].str.lower() 
+grpd = df.groupby('Character')
+
+#file = open("character_list.out",'w')
+#file.write(str(grpd.count()))
 #file.close()
 
-#print(df.groupby('Character').count())
-#print(df.loc[df['Character'] == 'Finn'].count())
-#print(df.loc[df['Character'] == 'Princess Bubblegum'].count())
-#print(len(df.loc[df['Character'] == 'Princess Bubblegum']))
-
 #### FUNCTIONS FOR CLEANING DATA ####
-def combine_names(ex1,ex2):
-    ### combine rows w/ Character attributes of ex1 and ex2
-    ### need to do a combo of groupby, aggregate, and (outer) join @_@ 
-    ### groupby['Character'], then aggregate 
+def combine_names(ex1,ex2,which_name):
+    ### combine rows w/ Character attributes of ex1 and ex2 (these args are strings)
+    new_df = pd.concat([df.loc[df['Character'] == ex1],df.loc[df['Character'] == ex2]])
+    if which_name == 1:
+        new_df.loc[new_df['Character'] == ex2, 'Character'] = ex1
+    elif which_name == 2:
+        new_df.loc[new_df['Character'] == ex1, 'Character'] = ex2
+    else:
+        print("Improper which_name value! Must be 1 or 2.")
+    return new_df
+
+def combine_names_in_list(list_of_grps,which_name):
+    list_of_dfs = [df.loc[df['Character'] == x] for x in list_of_grps]
+    new_df = pd.concat(list_of_dfs)
+    limit_which_name = len(list_of_grps)
+    if which_name <= limit_which_name: 
+        for i in range(limit_which_name):
+            new_df.loc[new_df['Character'] == list_of_grps[i], 'Character'] = list_of_grps[which_name]
+    else:
+        print("Improper which_name value!")
+    return new_df
+
+banana_list = ['banana guard','another banana guard', 'captain banana guard', 'female banana guard', 'private banana guard','the four banana guards','banana guards','banana guard #1','banana guard #2','banana guard (on radio)','banana guard 500',"banana guard [in finn's memory]",'banana guard [over phone]','banana guard i','banana guard ii','banana guard leader','banana guard number 3','flying banana guard', 'flying banana guards']
+banana_df = combine_names_in_list(banana_list,0)
+
+voice_list = ['a voice','a voice in the distance']
+voice_df = combine_names_in_list(voice_list,0)
+
+alarm_list = ['alarm','alarm clock bird','alarm clock with a headshot of princess bubblegum taped to the minute hand']
+alarm_df = combine_names_in_list(alarm_list,0)
+
+alien_list = ['alien groom','aliens']
+alien_df = combine_names_in_list(alien_list,0)
+
+townppl_list = ['townspeople (all)','townsperson','villager','villager with eyepatch','townsperson (blue shirt)','townsperson (green shirt)','townsperson (red shirt)','townsperson with fire-spitting belly button']
+townppl_df = combine_names_in_list(townppl_list,1)
+
+syrup_guard_list = ['syrup guard','syrup guard #2','syrup guard #3','syrup guard #4']
+syrup_df = combine_names_in_list(syrup_guard_list,0)
+
+susan_list = ['susan','susan strong']
+susan_df = combine_names_in_list(susan_list,1)
+
+softppl_list = ['soft people','soft child','soft person #1','soft person #2', 'soft person #3','old soft person']
+softppl_df = combine_names_in_list(softppl_list,0)
+
+smo_list = ['smo 1','smo 2','smo 3','smo 4','smo 5','smo 6']
+smo_df = combine_names_in_list(smo_list,0)
+
+snail_lady_list = ['snail lady','snail ladies','snail lady #1','snail lady #2','snail lady #3','snail lady #4']
+snail_lady_df = combine_names_in_list(snail_lady_list,0)
+
+tart_guard_list = ['royal tart path guard 1','royal tart path guard 2','royal tart path guard 3','royal tart path guard 4','royal tart path guard 5','royal tart path guard 6','royal tart path guard 7','royal tart path guard 8']
+tart_guard_df = combine_names_in_list(tart_guard_list,0)
+
+pillow_list = ['pillow people','pillow child','pillow children']
+pillow_df = combine_names_in_list(pillow_list,0)
+
+penguin_list = ['penguin','penguin [on tape]','penguins']
+penguin_df = combine_names_in_list(penguin_list,0)
+
+magic_man_list = ['magic man','magic man [as jake]','magic man [quietly]']
+magic_man_df = combine_names_in_list(magic_man_list,0)
+
+lesser_flame_guard_list = ['lesser flame guards','lesser flame guard 1','lesser flame guard 2','lesser flame guard 3']
+lesser_flame_guard_df = combine_names_in_list(lesser_flame_guard_list,0)
+
 
 ## Beemo, BMO
 ## Betty, Betty Grof? 
@@ -47,7 +108,6 @@ def combine_names(ex1,ex2):
 ## Flame Guard [x]
 ## Flame People, Flame Person
 ## Flame Princess [x]
-## Flying Banana Guard, Flying Banana Guards
 ## Flying Lettuce Brother, Flying Lettuce Brothers
 ## Fox [X]
 ## Gnome [X]
@@ -79,16 +139,12 @@ def combine_names(ex1,ex2):
 ## Royal Tart Path Guard [X]
 ## Simon [X]
 ## Snail Lady [x]
-## Soft Person X (same as pillow ppl???)
 ## Spiky Guard, (s)
 ## Starchie, Starchie (on radio), Starchy
 ## Suitor [x]
 ## Susan, Susan Strong
-## Syrup Guard [x]
 ## T.V., TV
 ## Lich, The Lich
-## [ all the banana guards, if not prev. mentioned]
-## Townsperson, Townspeople [X]
 ## Tree Trunks
 ## Tree Witch, Tree Witch'
 ## Wildberry Guard [x]
--- adventure_dataframe.py
+++ adventure_dataframe.py
@@ -4,49 +4,163 @@ import time
 import os.path 
 from bs4 import BeautifulSoup
 import pandas as pd 
+from lxml import html
 import numpy as np
+import re
 
-len_anchors = 280
+len_anchors = 280#280
 
-eps = np.zeros(len_anchors*2000)
-lines = np.zeros(len_anchors*2000)
-names = np.zeros(len_anchors*2000)
-dialogue = np.zeros(len_anchors*2000)
-actions = np.zeros(len_anchors*2000)
+size = len_anchors*2000
+eps = ["" for x in range(size)]
+lines =  ["" for x in range(size)]
+names =  ["" for x in range(size)]
+dialogue =  ["" for x in range(size)]
+actions =  ["" for x in range(size)]
 
 end_of_last_k = 0 
 for i in range(len_anchors):
-    if os.path.exists('adventure_time_ep'+str(i)+'_redacted.html'):
-        html_script = open('adventure_time_ep'+str(i)+'_redacted.html','r')
-        print("****************************")
-        num_lines = sum(1 for line in html_script if len(line) > 1)
+    if os.path.exists('./transcripts/adventure_time_ep'+str(i)+'_redacted.html'):
+        #url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
+        #html = urllib.urlopen(url).read()
+        html = open('./transcripts/adventure_time_ep'+str(i)+'_redacted.html','r')
+        soup = BeautifulSoup(html, "lxml")
+        for script in soup(["script", "style"]):  # kill all script and style elements
+            script.extract()    # rip it out
+        text = soup.get_text()
+        text_by_line = text.split('\n')
+        text_no_breaks = [l for l in text_by_line if len(l) > 0]
+        num_lines = len(text_no_breaks)
+        line_count = 0
         for k in range(num_lines):
             eps[k + end_of_last_k] = i
             lines[k + end_of_last_k] = k
-            #print(eps[k + end_of_last_k],k + end_of_last_k)
-        line_count = 0 
-        for f in html_script:
-            if len(f) > 1:
+            if len(text_no_breaks[k]) > 1:
                 line_count = line_count + 1
-                f_ended = f[:-1]
+                #f_ended = f[:-1]
                 #print(f[:-1]) #need to exclude last character, as it somehow is another line/a large space 
-                #print(len(f.split('</b>:')))
-                split_up = f_ended.split('</b>:')
-                if len(split_up) == 1:
+                split_up = text_no_breaks[k].split(':',maxsplit=1)
+                this_lines_actions = []
+                words = []
+                #if len(split_up) == 1:
+                if re.match(r'^\W',text_no_breaks[k]):
                     person = 'Narrator'
-                    words = split_up[0]
+                    #print(text_no_breaks[k])
+                    #words.append(split_up[0])
+                    words.append(text_no_breaks[k])
+                    this_lines_actions.append(words)
+                    #actions[line_count + end_of_last_k] = words
+                    #print(person,line_count)
                 elif len(split_up) == 2:
                     person = split_up[0]
-                    words = split_up[1]
                 else:
-                    print("Error in split!")
+                    print("Error in split!",i,k,text_no_breaks[k])
                 names[line_count + end_of_last_k] = person
-                dialogue[line_count + end_of_last_k] = words
+                more_words = []
+                if not 'Narrator' in person:
+                    split_by_italics = split_up[1].split('[')
+                    #print(len(split_by_italics),split_by_italics)
+                    if len(split_by_italics) > 1:
+                        for w in range(len(split_by_italics)): #len > 1 indicates actions are present
+                            if ']' in split_by_italics[w]:
+                                an_action = split_by_italics[w].split(']') 
+                                #print(k,w,an_action)
+                                if len(an_action[0]) > 0:
+                                    #print(an_action[0])
+                                    thing = []
+                                    thing.append(an_action[0])
+                                    this_lines_actions.append(thing)
+                                if len(an_action[1]) > 0:
+                                    #print(an_action[1])
+                                    thing = []
+                                    thing.append(an_action[1])
+                                    #print(more_words)
+                                    #print(len(an_action[1]),an_action[1])
+                                    more_words.append(thing)
+                                    #print(more_words)
+                            else: 
+                                if len(split_by_italics[w]) > 1:
+                                    #print(len(split_by_italics[w]),split_by_italics[w])
+                                    this_thing = []
+                                    this_thing.append(split_by_italics[w])
+                                    more_words.append(this_thing)
+                                    #print(more_words)
+                    elif len(split_by_italics) == 1:
+                        #print(i,k,split_by_italics)
+                        more_words.append(split_by_italics)
+                if len(more_words) == 1:
+                    #print(more_words)
+                    dialogue[line_count + end_of_last_k] = more_words[0]
+                elif len(more_words) > 1:
+                    no_spaces = []
+                    for y in range(len(more_words)):
+                        if len(more_words[y][0]) > 2:
+                            #print(i,k,y)
+                            no_spaces.append(more_words[y])
+                    dialogue[line_count + end_of_last_k] = no_spaces
+                if len(this_lines_actions) == 1:
+                    actions[line_count + end_of_last_k] = this_lines_actions[0]
+                elif len(this_lines_actions) > 1:
+                    actions[line_count + end_of_last_k] = this_lines_actions
+                #print("character for line ",k,": ",names[line_count + end_of_last_k])
+                #print("words for line ",k,": ",dialogue[line_count + end_of_last_k])
+                #print("actions for line ",k,": ",actions[line_count + end_of_last_k]) 
+                #print("line ",k,": ",names[line_count + end_of_last_k],": ",dialogue[line_count + end_of_last_k],actions[line_count + end_of_last_k])                      
         end_of_last_k = k + end_of_last_k
-        html_script.close()
-                ## column 2: f[0] w/o <b> tags, colon 
-                ## column 3:  
 
 
+#### using data constructed above, let's make a dataframe!
+eps = eps[:end_of_last_k+1]
+eps = np.asarray(eps).T 
+lines = lines[:end_of_last_k+1]
+lines = np.asarray(lines).T
+names = names[:end_of_last_k+1]
+dialogue = dialogue[:end_of_last_k+1]
+actions = actions[:end_of_last_k+1]
+names = np.asarray(names).T 
 
-        #ep_dataframe = pd.Dataframe()
+#for l in range(len(dialogue)):
+#    if len(dialogue[l]) > 3:
+#        print(len(dialogue[l]),dialogue[l])
+#for l in range(len(actions)):
+#    print(len(actions[l]),actions[l])
+
+data = [[None]*5 for i in range(end_of_last_k+1)]
+for i in range(len(data)):
+    data[i][0] = eps[i]
+    data[i][1] = lines[i]
+    data[i][2] = names[i]
+    if len(dialogue[i]) > 1:
+        ## INSTEAD: CONCATENATE ELEMENTS OF IT
+        #data[i][3] = tuple(dialogue[i])
+        s = ', '
+        data[i][3] = s.join(["".join(x) for x in dialogue[i]])
+        #print(data[i][3])
+    elif len(dialogue[i]) == 1:
+        #data[i][3] = dialogue[i][0]
+        data[i][3] = str(dialogue[i][0])
+    elif len(dialogue[i]) == 0:
+        data[i][3] = ""
+    if len(actions[i]) > 1:
+        ## INSTEAD: CONCATENATE ELEMENTS OF IT
+        #data[i][4] = tuple(actions[i])
+        s = ', '
+        data[i][4] = s.join(["".join(x) for x in actions[i]])
+        #print(data[i][4])
+    elif len(actions[i]) == 1:
+        #data[i][4] = actions[i][0]
+        data[i][4] = str(actions[i][0])
+    elif len(actions[i]) == 0:
+        data[i][4] = ""
+    #print(len(dialogue[i]),len(actions[i]))
+    #data[i][0] = tuple(eps[i])
+    #data[i][1] = tuple(lines[i])
+    #data[i][2] = tuple(names[i])
+    #data[i][3] = tuple(dialogue[i])
+    #data[i][4] = tuple(actions[i])
+    data[i] = tuple(data[i])
+    #print(i,len(data[i]))
+#print(len(data))
+print(data[219])
+df = pd.DataFrame(data,columns = ['Episode','Line','Character','Dialogue','Action'])
+#print(df.dtypes)
+df.to_pickle("./adventure_pickle_tupled.pkl")
